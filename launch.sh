f168dc7ffc1328d114204e9ee63cb7ec4c631c52
souravsanyal06 <souravsanyal06@github.com>
2025-05-07 14:33:15 -0400

    changes

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source /opt/ros/noetic/setup.bash
-source /home/sourav/safe_ai/cv_ws/devel/setup.bash
+source /home/sourav/ASMA/ros_ws/devel/setup.bash
 source ~/killross.sh
 roslaunch rotors_gazebo test_new.launch lockstep:=true &
 rosrun keyboard keyboard &
